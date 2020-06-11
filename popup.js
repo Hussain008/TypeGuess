549b5add7165096d6aeb6494ffe04303167379f5
Hussain Electricwala <hussainelectricwala8@gmail.com>
2020-06-11 07:54:32 +0530

    fetching is done and api is working correctly

--- popup.js
+++ popup.js
@@ -1,7 +1,47 @@
-var superImportantText='lol';
+var superImportantText='';
+var pageText = 'hi';
+
+
 chrome.storage.local.get(['content'], function(items) {
-	var content = items['content'].replace(/<[^>]+>/g, '');
-	superImportantText = content;
-	
-      console.log( "Here is the superImportant Text " + content);
-    });
+
+var content = items['content'].replace(/<[^>]+>/g, '').replace(/(\r\n|\n|\r)/gm,"");
+superImportantText = content;
+pageText = content;
+
+start();
+
+});
+
+
+function selectRandomWord ( words ) {
+	randomIndex = Math.floor((Math.random()*1000)%words.length) ;
+	console.log(words);
+	console.log(randomIndex);
+	console.log(words[randomIndex]);
+	return words[randomIndex];
+}
+
+function start(){
+	words = pageText..split(" ") ;
+	word = selectRandomWord(words);
+
+	$.ajax({
+        url: 'https://owlbot.info/api/v4/dictionary/'+word,
+        beforeSend: function(xhr) {
+             xhr.setRequestHeader("Authorization", "Token e4df540ec3454d5faa14ccf851905a339615dbb3")
+        }, success: function(data){
+        	if(data['definitions']===undefined)
+        		return;
+        	superImportantText =  data['definitions'][0]['definition'];
+        	test(superImportantText);
+        }})
+
+}
+
+function test(loll){
+	document.write(loll);
+}
+
+
+
+
